90d26554f22ac17d07744ba388182ad57695cd1c
Eliseev91 <eliseevfilipp91@gmail.com>
2019-12-14 18:50:39 +0300

    Реализованы функции peek и clear

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -5,42 +5,51 @@ from typing import Any, List
 
 _stack: List = []
 
-def push(elem: Any) -> None:
-	"""
-	Operation that add element to stack
 
-	:param elem: element to be pushed
-	:return: Nothing
-	"""
-	_stack.append(elem)
-	print(elem)
-	return None
+def push(elem: Any) -> None:
+    """
+    Operation that add element to stack
+
+    :param elem: element to be pushed
+    :return: Nothing
+    """
+    _stack.append(elem)
+    print(elem)
+    return None
 
 
 def pop() -> Any:
-	"""
-	Pop element from the top of the stack. If not elements - should return None.
+    """
+    Pop element from the top of the stack. If not elements - should return None.
 
-	:return: popped element
-	"""
-	return _stack.pop()
+    :return: popped element
+    """
+    if not _stack:
+        return None
+    else:
+        return _stack.pop()
 
 
 def peek(ind: int = 0) -> Any:
-	"""
-	Allow you to see at the element in the stack without popping it.
+    """
+    Allow you to see at the element in the stack without popping it.
 
-	:param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
-	:return: peeked element or None if no element in this place
-	"""
-	print(ind)
-	return None
+    :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
+    :return: peeked element or None if no element in this place
+    """
+
+    print(ind)
+    if not _stack:
+        return None
+    elif ind in range(len(_stack)):
+        ind = -(ind + 1)
+        return _stack[ind]
 
 
 def clear() -> None:
-	"""
-	Clear my stack
+    """
+    Clear my stack
 
-	:return: None
-	"""
-	return None
+    :return: None
+    """
+    return _stack.clear()
